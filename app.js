ac596d272345a321a4ed31ceaddf71b556331d13
kappasan21 <134040762+kappasan21@users.noreply.github.com>
2025-01-30 19:05:54 +0900

    Updates: Menu Page functions, Admin Page
    1. Update Menu Page - Add buttons to access Web Apps, display messages
    2. Fix issues about JWT token
    3. Add Admin Page (no-functions), and a button to access there

--- app.js
+++ app.js
@@ -14,15 +14,23 @@ require("dotenv").config();
 // For bcrypt encryption
 const bcrypt = require("bcrypt");
 
+
+
+
+
 // Assign the server port saved in .env file
 const PORT = process.env.PORT;
 
 // Create the instance of express
 const app = express();
+
+
 // List for origins to access this server
 const allowedOrigins = [
   "http://localhost:8510", // For this server
   "http://localhost:7654", // For Task Management App
+  "http://localhost:6200", // For Film Finder App
+  "http://localhost:7678",
   "https://supabase.com",
   "https://github-desktop-test-1.onrender.com", // Without trailing slash /
 ];
@@ -40,10 +48,10 @@ const corsOptions = {
   allowedHeaders: ["Content-Type", "Authorization"],
   credentials: true,
 };
-
 // Set the options configured above to CORS
 app.use(cors(corsOptions));
 // Enable to use cookieParse
+
 app.use(cookieParser());
 // Enable to access public both server and browser
 app.use(express.static("public"));
@@ -57,6 +65,11 @@ app.set("views", path.join(__dirname, "views"));
 // Secret key for JWT
 const SECRET_KEY = process.env.JWT_SECRET_KEY;
 
+
+// Import sql in db.js in oder to use sql connection string instead of the ordinary SQL connection settings below
+// const pool = require('./db.js');
+
+// PostgreSQL connection settings
 const pool = new Pool({
   user: process.env.DB_USER,
   host: process.env.DB_HOST,
@@ -66,7 +79,7 @@ const pool = new Pool({
   ssl: { rejectUnauthorized: false },
   keepAlive: true,
 });
-// Test the connection
+// Check the connection with Supabase
 (async () => {
   try {
     const client = await pool.connect();
@@ -82,9 +95,37 @@ const pool = new Pool({
 
 
 
+// Middleware to validate JWT
+const authenticateToken = (req, res, next) => {
+  console.log("cookies: ", req.cookies.token);
+  console.log("token saved in cookie: ", req.cookies.token);
+  // Enable to handle req patters either req.cookies, req.cookies.token, req.header['Authorization'], or req.headers['Authorization'].split(' ')[1]
+  // ...split(' ').[1] takes req.headers - "Authorization: Bearer JWT_TOKEN". So, take index 1 from Bearer = 0, _ , JWT_TOKEN = 1
+  const token = req.cookies?.token || req.headers['Authorization']?.split(' ')[1];
+
+  // case: no token
+  if(!token) {
+    res.locals.message = "Access denied.";
+    return res.status(401).redirect('/login');
+  }
+
+  // Validate JWT token
+  jwt.verify(token, SECRET_KEY, (err, user) => {
+    // case: invalid token
+    if(err) {
+      res.locals.message = "Invalid JWT token.";
+      return res.status(403).redirect('/login');
+    }
+    // case: valid token
+    console.log("Passed JWT token validation!");
+    req.user = user;
+    next();
+  });
+};
 
 
-// Disable any caches without configuration by code
+
+// Disable any kind of caches without configuration 
 app.use((req, res, next) => {
   res.setHeader(
     "Cache-Control",
@@ -92,6 +133,10 @@ app.use((req, res, next) => {
   );
   next();
 });
+
+
+
+
 
 // ROUTES
 // redirect to /login
@@ -112,6 +157,7 @@ app.get("/login", (req, res) => {
   });
 });
 
+
 // Display Signup Page
 app.get("/signup", (req, res) => {
   res.locals.login = false;
@@ -125,21 +171,34 @@ app.get("/signup", (req, res) => {
 });
 
 // Display Menu Page
-app.get("/menu", (req, res) => {
-  console.log("app.get('/menu');");
-  if (!res.locals.login) {
-    res.redirect("/login");
-    res.locals.page = "Log In";
-    res.locals.message = "Please log in, again...";
-  } else {
-    res.locals.page = "";
-    res.locals.message =
-      "Welcome, " + req.cookies.username + "! You can access the menu now.";
+app.get("/menu", authenticateToken, (req, res) => {
+  console.log("Path to app.get('/menu');");
+  console.log("req.user ", req.user);
+  
+  res.locals.login = true;
+  res.locals.page = "menu";
+  res.locals.message =
+    "Welcome, " + req.user.username + "! You can access one of apps listed below.";
+  
     res.render("menu", {
-      title: "Menu Page",
-      username: req.cookies.username,
-    });
-  }
+    title: "Menu Page",
+    username: req.user.username,
+  });
+});
+
+
+// Admin Page Path
+app.get('/admin', authenticateToken, (req, res) => {
+  console.log("Path to app.get('/admin');");
+  
+  res.locals.page = "admin";
+  res.locals.message = "You can add or remove the apps where users can access here.";
+  // Needs to re-assign later
+  res.locals.appList = []; 
+  res.render("adminMenu", {
+    title: "Admin Page",
+    username: req.user.username,
+  });
 });
 
 // Login Process:
@@ -190,26 +249,27 @@ app.post("/login", (req, res) => {
           // Check if hash in DB and password match
           if (isEqual) {
             // Generate JWT token
-            const token = jwt.sign({ id: user.id }, SECRET_KEY, {
+            const token = jwt.sign({ id: user.id, username: user.username, email: user.email }, SECRET_KEY, {
               expiresIn: "1h",
             });
-            console.log("Check point!!!");
-            // Set cookie
-            res.cookie("token", token, { httpOnly: true });
-            console.log("token: ", token);
+            console.log("Token just generated: ", token);
+            // Save cookie settings with JWT token
+            res.cookie("token", token, { httpOnly: true, secure: false });
+
             // Set vars for GUI
             res.locals.login = true;
-            res.locals.page = "";
+            res.locals.page = "menu";
             res.locals.message =
               "Welcome, " +
               user.username +
               "! You can access the menu now.\n\n" +
               "You received cookie token now: " +
               token;
-            res.status(200).render("menu.ejs", {
-              title: "Menu Page",
-              username: user.username,
-            });
+            // res.status(200).render("menu.ejs", {
+            //   title: "Menu Page",
+            //   username: user.username,
+            // });
+            res.status(200).redirect('/menu');
           } else {
             res.locals.login = false;
             res.locals.page = "login";
